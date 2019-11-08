46e1a41ef206097af69e7efdf4904646d9882689
Samuel Shumake <shumakesam@gmail.com>
2019-11-08 16:17:24 -0600

    Initial Commit

---
+++ GitGud/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitGud
+//
+//  Created by Samuel Shumake on 11/8/19.
+//  Copyright © 2019 Samuel Shumake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
