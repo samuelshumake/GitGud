686c8226611f50c08612829b69c04c86fc8d3f39
Samuel Shumake <shumakesam@gmail.com>
2019-11-08 22:57:25 -0600

    Parsing through JSON for each commit

--- GitGud/RepoData.swift
+++ GitGud/RepoData.swift
@@ -15,11 +15,13 @@ protocol RepoDataProtocol {
 
 class RepoData{
     private let urlSession = URLSession.shared
-    private let urlPathBase = "https://api.github.com/repos/samuelshumake/GitGud/branches/dev"
+    private let urlPathBase = "https://api.github.com/repos/samuelshumake/GitGud/commits?sha=dev"
     private var dataTask: URLSessionDataTask? = nil
     var delegate: RepoDataProtocol? = nil
 
     init() {}
+    
+    var gitSHA: Array<String> = []
     
     func getRepoData() {
         let urlPath = self.urlPathBase
@@ -30,9 +32,16 @@ class RepoData{
                 print(error!)
             } else {
                 do {
-                    let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary
-                    let repoDict = jsonResult!["commit"]! as? NSDictionary
-                    print(repoDict!["commit"])
+                    let data = NSData(contentsOf: URL(string: self.urlPathBase)!)
+                    let jsonResult = try JSONSerialization.jsonObject(with: data! as Data, options: JSONSerialization.ReadingOptions.mutableContainers)
+                    let branch = jsonResult as? [Dictionary<String, Any>]
+                    for i in branch! {
+                        let commit = i["commit"] as? NSDictionary
+                        let commitMessage = commit!["message"]!
+                        let commitAuthorDict = commit!["author"]! as? NSDictionary
+                        let commitSHA = i["sha"]
+                    }
+                    
                 } catch {
                     //Catch and handle the exception
                 }
