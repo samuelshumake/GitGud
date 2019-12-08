d7bf351548bf44c2441538c37370675bf2ca80e7
Samuel Shumake <shumakesam@gmail.com>
2019-12-07 19:51:24 -0600

    Deleted Scroll View files and created cell information controller

---
+++ GitGud/CommitCellViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CommitCellViewController.swift
+//  GitGud
+//
+//  Created by Samuel Shumake on 12/7/19.
+//  Copyright © 2019 Samuel Shumake. All rights reserved.
+//
+
+import UIKit
+
+class CommitCellViewController: UIViewController, UINavigationControllerDelegate {
+        
+        var cellName: String = "Blank"
+    
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view, typically from a nib.
+        }
+
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+
+
+    }
--- GitGud/RepoDataTableViewController.swift
+++ GitGud/RepoDataTableViewController.swift
@@ -78,14 +78,25 @@ class RepoDataTableViewController: UITableViewController {
     }
     */
 
-    /*
     // MARK: - Navigation
+     
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination
+         // Pass the selected object to the new view controller.
+         
+        if segue.identifier == "CellDetail" {
+             
+            let contentViewController = segue.destination as? CommitCellViewController
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+            if let selectedCell = sender as? CustomRepoTableViewCell {
+                let indexPath = tableView.indexPath(for: selectedCell)!
+                let name = repoInfo["dev"]![indexPath.row].message
+                if (contentViewController != nil) {
+                    contentViewController!.cellName = name
+                }
+            }
+        }
     }
-    */
+
 
 }
